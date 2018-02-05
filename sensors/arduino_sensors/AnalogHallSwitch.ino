153a6af00184a24741de8df54b57f654cfb82c80
Kasra Torshizi <gtorshizi@gmail.com>
2018-02-05 17:11:01 -0500

    Add arduino test programs for temperature and hall effect sensors

---
+++ sensors/arduino_sensors/AnalogHallSwitch.ino
@@ -0,0 +1,10 @@
+//Analog Hall Switch Testing Code
+const int hallAnalogPin=A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(hallAnalogPin));
+}
---
+++ sensors/arduino_sensors/TemperatureSensor.ino
@@ -0,0 +1,15 @@
+//AnalogTemperature Sensor
+
+const int tempAnalogPin=A1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int rawReading = analogRead(tempAnalogPin);
+  float voltage = rawReading*5.0/1024.0;
+  float temperatureC = (voltage-0.5)*100;
+  Serial.print("Degrees in C: ");
+  Serial.println(termperatureC);
+}
